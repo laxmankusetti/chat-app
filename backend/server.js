d48268f694c727f1526edfb3926df8e696f6f916
laxmankusetti <laxmankusetti@gmail.com>
2024-02-04 12:33:49 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -9,9 +10,11 @@ import { app, server } from './socket/socket.js';
 
 import connectToMongodb from './db/connectToMongodb.js';
 
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
-const port = process.env.PORT || 5000
 // const app = express();
 
 app.use(express.json());
@@ -20,6 +23,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/message', messageRouter);
 app.use('/api/users', userRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 server.listen(port, () => {
     connectToMongodb();
